6956cea18415d855100125a64ce9276ebd7d8b9d
Bernadette Januska <bmjanuska@gmail.com>
2018-08-18 16:35:44 -0400

    Connecting to database through storeCustomer

--- seeds.sql
+++ seeds.sql
@@ -8,3 +8,6 @@ VALUES ("Basil Plant","Herb",3.25,15);
 
 INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("Monstera","Decor",15.75,10);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Philodendron","Decor",12.99,10);
--- storeCustomer.js
+++ storeCustomer.js
@@ -1,16 +1,28 @@
 //====== REQUIRE
-
-
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 //==============
 
 
 //====== CONNECTIONS
-
-
+//--- Var to Connect to DB
+var connection = mysql.createConnection ({
+	host: "localhost",
+	port: 8889, 
+	user: "root",
+	password: "root", 
+	database: "sqlstore_DB"
+});
+//--- Connect to server and DB
+connection.connect(function (err){
+	if(err) throw err; 
+	startShopping(); 
+}); 
 //==================
 
-
 //====== FUNCTIONS
+function startShopping (){
 
+} 
 
 //================
