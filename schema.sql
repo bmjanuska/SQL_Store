4fd14a0c4f10c569ab4898e1879ee6b2cd80cd39
Bernadette Januska <bmjanuska@gmail.com>
2018-08-18 10:32:56 -0400

    Initial file upload

---
+++ schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS sqlstore_DB; 
+
+CREATE DATABASE sqlstore_DB; 
+
+USE sqlstore_DB; 
+
+CREATE TABLE products(
+	-- * item_id (unique id for each product)
+	item_id INT(10) NOT NULL AUTO_INCREMENT,
+	-- * product_name (Name of product)
+	product_name VARCHAR(225) NOT NULL,
+	-- * department_name
+	department_name VARCHAR(225) NOT NULL,
+	-- * price (cost to customer)
+	price DECIMAL(10.2) NOT NULL,
+	-- * stock_quantity (how much of the product is available in stores)
+	stock_quantity INT(10) NOT NULL,
+	PRIMARY KEY (item_id)
+)
---
+++ seeds.sql
@@ -0,0 +1,10 @@
+--- MOCK DATA
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Thyme Plant","Herb",4.25,15);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Basil Plant","Herb",3.25,15);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Monstera","Decor",15.75,10);
